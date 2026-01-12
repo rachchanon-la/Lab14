4378ba6430835ca6863039c1ad17113ed5faa96a
rachchanon-la <rachchanon_la@cmu.ac.th>
2026-01-13 00:32:51 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[], int N){
+    for(int i = 1; i < N; i++){
+        T key = d[i];
+        int j = i - 1;
 
-}
+        while(j >= 0 && d[j] < key){
+            d[j + 1] = d[j];
+            j--;
+        }
+        d[j + 1] = key;
 
-int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
-	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
-	cout << "\n\n";
-	
-	insertionSort(a,10);
-	
-	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+        cout << "Pass " << i << ":";
+        for(int k = 0; k < N; k++){
+            cout << d[k] << " ";
+        }
+        cout << endl;
+    }
 }
